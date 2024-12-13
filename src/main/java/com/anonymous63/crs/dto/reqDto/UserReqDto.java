6d36c9abceb190d5d4b22879d5219e2241bcba21
anonymous-63 <anonymous63.krutik@gmail.com>
2024-12-13 08:47:12 +0530

    import dependancy

--- src/main/java/com/anonymous63/crs/dto/reqDto/UserReqDto.java
+++ src/main/java/com/anonymous63/crs/dto/reqDto/UserReqDto.java
@@ -4,5 +4,13 @@ import lombok.Data;
 
 @Data
 public class UserReqDto {
-    
+    private Long id;
+    private String userName;
+    private String firstName;
+    private String middeleName;
+    private String lastName;
+    private String email;
+    private String phoneNo;
+    // private Address address;
+    private String password;
 }
--- src/main/java/com/anonymous63/crs/model/User.java
+++ src/main/java/com/anonymous63/crs/model/User.java
@@ -3,11 +3,13 @@ package com.anonymous63.crs.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class User {
+    @Id
     private Long id;
     private String userName;
     private String firstName;
@@ -15,7 +17,7 @@ public class User {
     private String lastName;
     private String email;
     private String phoneNo;
-    private Address address;
+    // private Address address;
     private String password;
 
     private LocalDateTime createAt;
